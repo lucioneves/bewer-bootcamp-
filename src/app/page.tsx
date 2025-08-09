9d058e4ce091cb4c2909b203d1434b45354129df
Lucio Neves <luciocecilia20@gmail.com>
2025-08-09 19:54:45 -0300

    feat: add product list and product item components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,45 @@
-import { Header } from "@/components/commom/header";
+import Image from "next/image";
 
-const Home = () => {
+import { Header } from "@/components/commom/header";
+import ProductList from "@/components/commom/product-list";
+import { db } from "@/db";
+
+const Home = async () => {
+  const products = await db.query.productTable.findMany({
+    with: {
+      variants: true,
+    },
+  });
+  console.log(products);
+
   return (
     <>
       <Header />
+      <div className="space-y-6">
+        <div className="px-5">
+          <Image
+            src="/banner-01.png"
+            alt="Leve uma vida com estilo"
+            height={0}
+            width={0}
+            sizes="100vw"
+            className="h-auto w-full"
+          />
+        </div>
+
+        <ProductList products={products} title="Mais Vendidos" />
+
+        <div className="px-5">
+          <Image
+            src="/banner-02.png"
+            alt="Leve uma vida com estilo"
+            height={0}
+            width={0}
+            sizes="100vw"
+            className="h-auto w-full"
+          />
+        </div>
+      </div>
     </>
   );
 };
